eba2a36098aaf3e5413e30e10ecdabcc80d6b57d
E <afftar@list.ru>
2020-02-03 19:58:37 +0700

    some verbosity added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import logging
 import subprocess
 from time import sleep
+from datetime import datetime as dt
 
 from win10toast import ToastNotifier
 
@@ -10,6 +12,7 @@ from utils import is_new, add_db_entry
 from youla import YoulaSearch
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - [%(levelname)s] %(message)s')
     db.create_tables([Ad], safe=True)
 
     av = AvitoSearch(QUERY)
@@ -22,13 +25,16 @@ if __name__ == '__main__':
     while True:
         items = y.check_new()
         items.extend(av.check_new())
-
+        if not items:
+            print(f"[{dt.strftime(dt.now(), '%H:%M:%S')}] No ads posted at all", end='\r')
         for x in items:
             ad_url = x['url']
             ad_title = x['title']
 
             if not is_new(ad_url, ad_title):
+                print(f"[{dt.strftime(dt.now(), '%H:%M:%S')}] No new ads posted", end='\r')
                 continue
+            logging.critical(f'New ad: {ad_url}')
             add_db_entry(ad_url, ad_title)
 
             toaster.show_toast(
